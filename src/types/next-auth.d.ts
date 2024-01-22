3ba384432489c86111fe7878dffb2fb61b5bc21a
Rajath U Shetty <rajathushetty56@gmail.com>
2024-01-22 23:06:53 +0530

    added user-profile

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,18 +1,20 @@
-import type { Session, User } from "next-auth"
-import { JWT } from "next-auth/jwt";
+import type { Session, User } from 'next-auth'
+import type { JWT } from 'next-auth/jwt'
 
 type UserId = string
 
 declare module 'next-auth/jwt' {
-    interface JWT {
-        id: UserId
-        username?: string | null
-    }
+  interface JWT {
+    id: UserId
+    username?: string | null
+  }
 }
 
 declare module 'next-auth' {
-    interface Session {
-        id: UserId
-        username?: string | null
+  interface Session {
+    user: User & {
+      id: UserId
+      username?: string | null
     }
+  }
 }

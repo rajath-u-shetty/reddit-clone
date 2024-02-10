b2239f1ee4155b751cb5e22b2d45c40083198145
Rajath U Shetty <rajathushetty56@gmail.com>
2024-02-10 09:23:49 +0530

    added subreddits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from '@/components/Navbar'
+import Providers from '@/components/Providers'
 import { Toaster } from '@/components/ui/Toaster'
 import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
@@ -25,17 +26,18 @@ export default function RootLayout({
       'bg-white text-slate-900 antialiased light', 
       inter.className)}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
-        {/* @ts-expect-error server component */}
-        <Navbar />
+          <Providers>
+            {/* @ts-expect-error server component */}
+            <Navbar />
 
-        {authModel}
-        
-        <div className='container max-w-7xl mx-auto h-full pt-12'>
-          {children}
-        </div>
-
-        <Toaster />
-        </body>
+            {authModel}
+            
+            <div className='container max-w-7xl mx-auto h-full pt-12'>
+              {children}
+            </div>
+            <Toaster />
+          </Providers>
+      </body>
     </html>
   )
 }
---
+++ src/lib/validators/subreddit.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const subredditvalidator = z.object({
+    name: z.string().min(3).max(21),
+})
+
+export const subredditsubscriptionvalidator = z.object({
+    subredditId: z.string()
+})
+
+export type CreateSubredditPayload = z.infer<typeof subredditvalidator>
+export type SubscrbeToSubredditPayload = z.infer<typeof subredditsubscriptionvalidator>
